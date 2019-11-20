b5d0f75258f1b47afa43f2b2402a0faaff103cac
KrisiKostadinov <krisi.199898@gmail.com>
2019-11-20 15:55:16 +0200

    Added contacts

---
+++ Visualalgo/js/header.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('#login-btn').click(function() {
+        $('.login').fadeIn(200);
+    });
+    $('.close-btn').click(function() {
+        $('.login').fadeOut(200);
+    });
+});
